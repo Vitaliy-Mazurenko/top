4df80d8723c902fd8b062bd863de9d307965d721
Igor Komendantov <komendantov2580@gmail.com>
2023-09-26 14:45:31 +0300

    color palette added to theme provider

--- src/app/providers/ThemeProvider/index.tsx
+++ src/app/providers/ThemeProvider/index.tsx
@@ -3,6 +3,19 @@ import { GlobalStyle } from './GlobalStyles'
 
 export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
 	const theme = {
+		palette: {
+			'primary': '#ff8c33',
+			'secondary-1': '#c6e1ec',
+			'secondary-2': '#e4afe7',
+			'secondary-3': '#e4afe7',
+			'secondary-4': '#f1e5a9',
+			'secondary-5': '#e3eeb6',
+			'secondary-6': '#b2f0e3',
+			'text': '#333333',
+			'tertiary': '#0076b3',
+			'alarm': '#0076b3'
+		},
+
 		typography: {
 			'Heading-1': css`
 		font-weight: 900;
@@ -49,9 +62,9 @@ export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
 	}
 
 	return (
-		<StyledProvider theme={theme}>
+		<StyledProvider theme={theme} >
 			<GlobalStyle />
 			{children}
-		</StyledProvider>
+		</StyledProvider >
 	)
 }
