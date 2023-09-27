56b38306860c35331df1be1fb630e90024a1b2d4
Igor Komendantov <komendantov2580@gmail.com>
2023-09-27 22:32:45 +0300

    styled wrapper for reusable modal

---
+++ src/shared/ui/MODAL_/ModalWrapper.ts
@@ -0,0 +1,16 @@
+export const ModalWrapper = styled.div<{ $center?: boolean }>`
+position: relative;
+top: 0;
+bottom: 0;
+left: 0;
+right: 0;
+background-color: rgba(0, 0, 0, 0.25);
+
+${props => props.$center &&
+	css`
+	display: flex;
+	justify-content: space-between;
+	align-content: ;
+`
+}
+`
