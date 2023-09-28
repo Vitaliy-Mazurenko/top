8182f4fea5057d7137dd7dbc3593933f9b7bf489
Igor Komendantov <komendantov2580@gmail.com>
2023-09-28 20:00:13 +0300

    commit

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,37 +1,27 @@
-// import { useState } from "react";
-// import AddCandidate from "../pages/profile/ui/AddCandidate/AddCandidate";
+import { useState } from "react";
+import AddCandidate from "../pages/profile/ui/AddCandidate/AddCandidate";
 
-// const App = () => {
-//   const [isAddCandidateOpen, setIsAddCandidateOpen] = useState(false);
-
-//   const openAddCandidate = () => {
-//     setIsAddCandidateOpen(true);
-//   };
-
-//   const closeAddCandidate = () => {
-//     setIsAddCandidateOpen(false);
-//   };
-
-//   return (
-//     <div>
-//       <button onClick={openAddCandidate}>Open Add Candidate</button>
-
-//
-//       <AddCandidate isOpen={isAddCandidateOpen} onClose={closeAddCandidate} />
-//     </div>
-//   );
-// };
-
-// export default App;
-import Table from "../pages/profile/ui/Table/Table";
-// import Main from "shared/ui/main/Main";
 const App = () => {
+  const [isAddCandidateOpen, setIsAddCandidateOpen] = useState(false);
+
+  const openAddCandidate = () => {
+    setIsAddCandidateOpen(true);
+  };
+
+  const closeAddCandidate = () => {
+    setIsAddCandidateOpen(false);
+  };
+
   return (
     <div>
-      <Table />
-      {/* <Main /> */}
+      <button onClick={openAddCandidate}>Open Add Candidate</button>
+
+
+      <AddCandidate isOpen={isAddCandidateOpen} onClose={closeAddCandidate} />
     </div>
   );
 };
+
+
 
 export default App;
