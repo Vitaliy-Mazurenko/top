91abecf2f066abc58b4dca75cf2e6a98dd9aa651
Igor Komendantov <komendantov2580@gmail.com>
2023-09-28 19:01:03 +0300

    edited mockData for TeamMembersList

--- src/pages/profile/ui/TeamMembersList/mockData.ts
+++ src/pages/profile/ui/TeamMembersList/mockData.ts
@@ -5,13 +5,12 @@ import node from 'shared/assets/icons/nodejs.svg'
 import java from 'shared/assets/icons/java.png'
 import js from 'shared/assets/icons/js.png'
 
-
 export const mockData: TeamMemberCardProps[] = [
 	{ experience: '1 year', name: 'Igor', position: 'Frontend-developer', projectsNumber: 3, speciality: 'React', teamNumber: 3, status: 'online', specialityIcon: mainSkillIcon, headerColor: '#5858b0', avatarURL: 'https://i.imgur.com/iTWSxHL.jpeg' },
-	{ experience: '6 mon', name: 'Igor', position: 'Frontend-developer', projectsNumber: 3, speciality: 'React', teamNumber: 3, status: 'online', specialityIcon: python, headerColor: 'red', avatarURL: 'https://i.imgur.com/iTWSxHL.jpeg' },
-	{ experience: '5 years', name: 'Igor', position: 'Frontend-developer', projectsNumber: 3, speciality: 'React', teamNumber: 3, status: 'online', specialityIcon: node, headerColor: 'darkgray', avatarURL: 'https://i.imgur.com/iTWSxHL.jpeg' },
-	{ experience: '12 years', name: 'Igor', position: 'Frontend-developer', projectsNumber: 3, speciality: 'React', teamNumber: 3, status: 'online', specialityIcon: java, headerColor: '#075b5b', avatarURL: 'https://i.imgur.com/iTWSxHL.jpeg' },
-	{ experience: '6 years', name: 'Igor', position: 'Frontend-developer', projectsNumber: 3, speciality: 'React', teamNumber: 3, status: 'online', specialityIcon: js, headerColor: '#bd7380', avatarURL: 'https://i.imgur.com/iTWSxHL.jpeg' },
-	{ experience: '1 mon', name: 'Igor', position: 'Frontend-developer', projectsNumber: 3, speciality: 'React', teamNumber: 3, status: 'online', specialityIcon: java, headerColor: 'black', avatarURL: 'https://i.imgur.com/iTWSxHL.jpeg' },
+	{ experience: '6 mon', name: 'Igor', position: 'Backend developer', projectsNumber: 3, speciality: 'Python', teamNumber: 3, status: 'online', specialityIcon: python, headerColor: 'red', avatarURL: 'https://i.imgur.com/iTWSxHL.jpeg' },
+	{ experience: '5 years', name: 'Igor', position: 'Backend developer', projectsNumber: 3, speciality: 'Node.js', teamNumber: 3, status: 'online', specialityIcon: node, headerColor: 'darkgray', avatarURL: 'https://i.imgur.com/iTWSxHL.jpeg' },
+	{ experience: '12 years', name: 'Igor', position: 'Frontend-developer', projectsNumber: 3, speciality: 'Java', teamNumber: 3, status: 'online', specialityIcon: java, headerColor: '#075b5b', avatarURL: 'https://i.imgur.com/iTWSxHL.jpeg' },
+	{ experience: '6 years', name: 'Igor', position: 'Frontend-developer', projectsNumber: 3, speciality: 'JavaScript', teamNumber: 3, status: 'online', specialityIcon: js, headerColor: '#bd7380', avatarURL: 'https://i.imgur.com/iTWSxHL.jpeg' },
+	{ experience: '1 mon', name: 'Igor', position: 'Backend developer', projectsNumber: 3, speciality: 'Java', teamNumber: 3, status: 'online', specialityIcon: java, headerColor: 'black', avatarURL: 'https://i.imgur.com/iTWSxHL.jpeg' },
 	{ experience: '1 mon', name: 'Igor', position: 'Frontend-developer', projectsNumber: 3, speciality: 'React', teamNumber: 3, status: 'online', specialityIcon: js, headerColor: '#5858b0', avatarURL: 'https://i.imgur.com/iTWSxHL.jpeg' }
 ]
