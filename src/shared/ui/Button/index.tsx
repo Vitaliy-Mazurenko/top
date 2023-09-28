f8a04ff7ce4b73617ed8ec5ef0dba64d4931cf05
Igor Komendantov <komendantov2580@gmail.com>
2023-09-29 01:34:02 +0300

    create button component

---
+++ src/shared/ui/Button/index.tsx
@@ -0,0 +1,19 @@
+import styled, { css } from "styled-components";
+
+	//если variant не указан вовсе, то по умолчанию он равен primary
+export const Button = styled.button<{ $variant?: 'primary' | 'secondary' }>`
+	min-width: 17.625rem;
+	padding: 1rem;
+	
+	${({ theme, $variant = 'primary' }) => $variant === 'primary' && css`
+	background-color: ${theme.palette.primary}; // если вариант primary, то задаем такой css
+	color: white;
+	`};
+
+	${({ theme, $variant = 'primary' }) => $variant === 'secondary' && css`
+	// если вариант secondary, то задаем такой css
+	background: none;
+	color: ${theme.palette.text};
+	border: 2px solid ${theme.palette.primary} 
+	`}
+`
