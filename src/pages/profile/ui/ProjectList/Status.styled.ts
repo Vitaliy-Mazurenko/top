17c2f8f36d451795296468795eba6fd534a10725
Igor Komendantov <komendantov2580@gmail.com>
2023-09-30 19:37:57 +0300

    Status component fix

--- src/pages/profile/ui/ProjectList/Status.styled.ts
+++ src/pages/profile/ui/ProjectList/Status.styled.ts
@@ -1,17 +1,16 @@
-import styled, { RuleSet } from "styled-components";
+import styled from "styled-components";
 
-export  const Status = styled.div<{ 
-	$css?: RuleSet 
-	$pointer?: boolean
-	}>`
+export const Status = styled.div<{
+	$backgroundColor?: string
+	$textColor?: string
+}>`
 	padding: 0.3125rem;
 	text-align: center;
 	font-size: 0.75rem;
 	margin-top: 0.625rem;
 	${({ theme }) => theme.typography['Heading-7']}
-	/* default */
 	color: ${props => props.theme.palette.text};
-	${props => props.$css};
-	cursor: ${props => props.$pointer ? 'pointer' : 'default'};
 	
+	${props => props.$backgroundColor && `backgroundColor: ${props.$backgroundColor}`}
+	${props => props.$textColor && `color: ${props.$textColor}`}
 `
