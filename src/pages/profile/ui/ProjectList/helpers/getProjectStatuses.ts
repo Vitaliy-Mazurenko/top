cc5eb802772c568fda282e9387db8b369c991b8c
Igor Komendantov <komendantov2580@gmail.com>
2023-09-30 19:49:54 +0300

    small fix Status component

--- src/pages/profile/ui/ProjectList/helpers/getProjectStatuses.ts
+++ src/pages/profile/ui/ProjectList/helpers/getProjectStatuses.ts
@@ -9,22 +9,22 @@ export const getProjectStatuses: () => Record<ProjectCardProps['status'], Status
 	},
 	testing: {
 		color: 'rgba(223, 20, 227, 1)',
-		background: 'rgba(223, 20, 227, 0.18)'
+		backgroundColor: 'rgba(223, 20, 227, 0.18)'
 	},
 	completed: {
 		color: 'rgba(230, 95, 43, 1)',
-		background: 'rgba(230, 95, 43, 0.18)',
+		backgroundColor: 'rgba(230, 95, 43, 0.18)',
 	},
 	development: {
 		color: 'rgba(13, 63, 124, 1)',
-		background: 'rgba(13, 63, 124, 0.18)',
+		backgroundColor: 'rgba(13, 63, 124, 0.18)',
 	},
 	canceled: {
 		color: 'rgba(223, 34, 9, 1)',
-		background: 'rgba(223, 34, 9, 0.18)',
+		backgroundColor: 'rgba(223, 34, 9, 0.18)',
 	},
 	frozen: {
 		color: 'rgba(66, 64, 73, 1)',
-		background: 'rgba(66, 64, 73, 0.18)',
+		backgroundColor: 'rgba(66, 64, 73, 0.18)',
 	}
 })
