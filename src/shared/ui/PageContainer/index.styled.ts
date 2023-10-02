73bc7826fbcc0114eabb34ec8a1fb11eebee9e3a
Igor Komendantov <komendantov2580@gmail.com>
2023-10-03 00:46:21 +0300

    move PageContainer to shared

---
+++ src/shared/ui/PageContainer/index.styled.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const PageContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: row;
+  height: 100dvh;
+  overflow: hidden;
+`;
