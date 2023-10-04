9c6094ef5b8c92159c1ccdc2c2db95956a1798a0
Igor Komendantov <komendantov2580@gmail.com>
2023-10-04 12:55:59 +0300

    fix routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,12 @@
 import { ProfilePage } from "pages/(profile)/index";
 import { ProfileSettingsPage } from "pages/(profile)/settings";
 import { DaschboardPage } from "pages/dashboard/ui/Daschboard/Daschboard";
-import { IndexPage } from "pages/index";
 import { TeamPage } from "pages/team";
 import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<IndexPage />} />
       <Route element={<TeamPage />} path="/team" />
      
       <Route element={<ProfilePage />} path="/profile" />
