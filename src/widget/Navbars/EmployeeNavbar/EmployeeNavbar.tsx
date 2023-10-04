bd6f84ede3249635ff8b9a377aaed9ea87871e28
Igor Komendantov <komendantov2580@gmail.com>
2023-10-04 16:33:54 +0300

    fix navbar

--- src/widget/Navbars/EmployeeNavbar/EmployeeNavbar.tsx
+++ src/widget/Navbars/EmployeeNavbar/EmployeeNavbar.tsx
@@ -6,13 +6,16 @@ import {
 import logoImg from "shared/assets/img/logo.png";
 import { PublicOptions } from "../GlobalOptions";
 import { EmployeeTabs } from "./EmployeeTabs";
+import { ProfileCard } from "entities/user";
+import defaultAvatar from 'shared/assets/img/sidebarimg.png'
 
 export const EmployeeNavbar = () => {
-  
+
   return (
     <SideMenuWrap>
       <LogoAndTabsWrap>
         <LogoWrap src={logoImg} />
+        <ProfileCard name="Повне ім'я" position="Посада" imgURL={defaultAvatar} />
         <EmployeeTabs />
       </LogoAndTabsWrap>
       <PublicOptions />
