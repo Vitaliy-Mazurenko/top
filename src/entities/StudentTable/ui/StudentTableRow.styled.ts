c633e11ae78bfc57a9ad3a851beb7434ca229658
Ivan Bilobrov <ivanbilobrov07@gmail.com>
2023-10-06 14:17:14 +0300

    add dashboard container

--- src/entities/StudentTable/ui/StudentTableRow.styled.ts
+++ src/entities/StudentTable/ui/StudentTableRow.styled.ts
@@ -3,20 +3,46 @@ import styled from "styled-components";
 import { UserCredentials } from "features/UserCredentials";
 
 export const StyledTableRow = styled.tr`
-  height: 64px;
+  height: 59px;
 
   td {
     padding: 8px;
     vertical-align: middle;
   }
 
-  &:last-child {
-    height: 85px;
+  @media screen and (min-width: 480px) {
+    height: 72px;
+
+    &:last-child {
+      height: 80px;
+    }
+
+    &:last-child td {
+      padding: 8px 8px 18px;
+    }
   }
 
-  &:last-child td {
-    vertical-align: baseline;
-    padding: 16px 8px;
+  @media screen and (min-width: 768px) {
+    &:last-child {
+      height: 82px;
+    }
+
+    &:last-child td {
+      padding: 8px 8px 20px;
+    }
+  }
+
+  @media screen and (min-width: 1080px) {
+    height: 64px;
+
+    &:last-child {
+      height: 85px;
+    }
+
+    &:last-child td {
+      vertical-align: baseline;
+      padding: 16px 8px;
+    }
   }
 `;
 
--- src/pages/dashboard/ui/StyledDaschboard.styled.ts
+++ src/pages/dashboard/ui/StyledDaschboard.styled.ts
@@ -1,12 +1,33 @@
 import styled from "styled-components";
 
 export const StyledDaschboard = styled.div`
-  max-height: 100%;
-  display: flex;
-  padding-right: 5.3%;
-  gap: 27px;
-  justify-content: space-between;
   font-family: "Inter", sans-serif;
+
+  @media screen and (min-width: 1080px) {
+    display: flex;
+    padding-right: 5.3%;
+  }
+`;
+
+export const DashboardContainer = styled.div`
+  width: 100%;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 0 10px;
+
+  @media screen and (min-width: 480px) {
+    width: 468px;
+  }
+
+  @media screen and (min-width: 768px) {
+    width: 724px;
+  }
+
+  @media screen and (min-width: 1080px) {
+    width: auto;
+    margin: 0;
+    padding: 0;
+  }
 `;
 
 export const LeftNavBar = styled.aside`
@@ -16,8 +37,10 @@ export const LeftNavBar = styled.aside`
 `;
 
 export const RightSideContent = styled.div`
-  margin-left: 227px;
-  flex-basis: 83.4%;
+  @media screen and (min-width: 1080px) {
+    margin-left: 227px;
+    flex-basis: 83.4%;
+  }
 
   @media screen and (min-width: 1450px) {
     margin-left: auto;
