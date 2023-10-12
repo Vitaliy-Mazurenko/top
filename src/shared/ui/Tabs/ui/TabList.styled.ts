8d211c162c9aadd1bfd2fa0b71431e410de07430
Vitaliy Mazurenko <vit007m@gmail.com>
2023-10-13 00:49:14 +0300

    removed: unnecessary code

--- src/shared/ui/Tabs/ui/TabList.styled.ts
+++ src/shared/ui/Tabs/ui/TabList.styled.ts
@@ -3,10 +3,4 @@ import styled from "styled-components";
 export const TabsWrap = styled.ul`
   display: flex;
   flex-direction: column;
-
-    & li:last-child {
-      /* @media screen and (min-width: 768px) {
-        display: none;
-    } */
-  }
 `;
