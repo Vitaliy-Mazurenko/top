63cb0c3e72ab2c8aa3c6c3482fe20e8a48e979f1
Vitaliy Mazurenko <vit007m@gmail.com>
2023-10-18 00:09:40 +0300

    moved: DashboardNavBar

---
+++ src/widget/Navbars/ui/DashboardNavBar/DashboardNavBar.tsx
@@ -0,0 +1,102 @@
+import { useMediaQuery } from "usehooks-ts";
+import { RefObject } from "react";
+
+import { TabList } from "shared/ui/Tabs";
+import { ITabInfo } from "shared/ui/Tabs/types/Tab";
+
+import {
+  AsideWrapper,
+  DashboardContent,
+  LogoAndTabsWrap,
+  LogoLink,
+  LogoWrap,
+} from "../NavBar.styled";
+
+import logoURL from "shared/assets/img/logo.svg";
+import settingsImg from "shared/assets/icons/settings.svg";
+import helpImg from "shared/assets/icons/orange-question-mark.svg";
+import userImg from "shared/assets/icons/user.svg";
+import efficientImg from "shared/assets/icons/arrow-up.svg";
+import walletImg from "shared/assets/icons/wallet.svg";
+import coursesImg from "shared/assets/icons/courses.svg";
+import calendar from "shared/assets/icons/calendar.svg";
+
+interface IManagerNavBarProps {
+  className?: string;
+  mobileMenuRef?: RefObject<HTMLElement>;
+}
+
+export const DashboardNavBar: React.FC<IManagerNavBarProps> = ({
+  className,
+  mobileMenuRef,
+}) => {
+  const isScreenWidthLessThan_480 = useMediaQuery("(max-width: 480px)");
+  const isScreenWidthLessThan_767 = useMediaQuery("(max-width: 767px)");
+
+  const managerTabsData: ITabInfo[] = [
+    {
+      to: "/",
+      text: "Панель",
+      leftIcon: userImg,
+    },
+    {
+      to: "/",
+      text: "Витрати",
+      leftIcon: walletImg,
+    },
+    {
+      to: "/",
+      text: "Ефективність компаній",
+      leftIcon: efficientImg,
+    },
+    {
+      to: "/",
+      text: "Порівняння крі",
+      leftIcon: userImg,
+    },
+    {
+      to: "/",
+      text: "Курси та тренінги",
+      leftIcon: coursesImg,
+    },
+  ];
+
+  const optionsTabsData: ITabInfo[] = [
+    {
+      to: "/",
+      text: "Допомога",
+      leftIcon: helpImg,
+    },
+    {
+      to: "/",
+      text: isScreenWidthLessThan_480 ? "Налаштунок" : "Налаштування",
+      leftIcon: settingsImg,
+    },
+    {
+      to: "/",
+      text: "Вийти",
+      leftIcon: userImg,
+    },
+  ];
+
+  isScreenWidthLessThan_767 &&
+    managerTabsData.push({
+      to: "/calendar",
+      text: "Календар",
+      leftIcon: calendar,
+    });
+
+  return (
+    <AsideWrapper ref={mobileMenuRef} className={className}>
+      <DashboardContent>
+        <LogoAndTabsWrap>
+          <LogoLink to="/">
+            <LogoWrap src={logoURL} alt="logo" />
+          </LogoLink>
+          <TabList tabsInfo={managerTabsData} />
+        </LogoAndTabsWrap>
+        <TabList tabsInfo={optionsTabsData} />
+      </DashboardContent>
+    </AsideWrapper>
+  );
+};
