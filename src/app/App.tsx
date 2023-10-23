8de02c547c8e74328ed31be7529f6e2744752e0d
Vitaliy Mazurenko <vit007m@gmail.com>
2023-10-23 17:13:00 +0300

    fix: App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -21,12 +21,12 @@ const App = () => {
       <Route element={<ManagerLayout />} path="/">
         <Route index element={<></>} />
         {/* <Route index element={<HomePage />} /> */}
-        <Route element={<></>} path="profile_admin" />
+        {/* <Route element={<></>} path="profile_admin" /> */}
         <Route element={<PersonalPage />} path="projects" />
         <Route element={<ProjectItemPage />} path="projects/:id" />
-        <Route element={<></>} path="notifications" />
+        {/* <Route element={<></>} path="notifications" /> */}
         <Route element={<TeamPage />} path="team" />
-        <Route element={<></>} path="team/:id" />
+        {/* <Route element={<></>} path="team/:id" /> */}
       </Route>
     </Routes>
   );
